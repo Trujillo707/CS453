92ed5f2c02fb7ca559cb676c611fbf80d6fa667b
Orlando Trujillo-Ortiz <orlandotrujillo573@gmail.com>
2024-09-25 21:53:19 -0700

    some stuff

---
+++ hw3/f24-facts/facts/FactsProject.java
@@ -0,0 +1,24 @@
+package facts;
+import javax.swing.*;
+
+public class FactsProject {
+    private JPanel contentPane;
+    private JLabel robertLGlassFactsLabel;
+    private JLabel bigQuoteLabel;
+    private JSeparator seperator1;
+    private JSeparator separator2;
+    private JButton nextRandomQuoteButton;
+    private JTextField searchBoxTextField;
+    private JLabel searchHistoryLabel;
+    private JLabel modeTextLabel;
+    private JRadioButton textModeRadioButton;
+    private JRadioButton authorModeRadioButton;
+    private JRadioButton typeModeRadioButton;
+    private JRadioButton allModeRadioButton;
+    private JButton searchButton;
+    private JButton resetButton;
+    private JLabel yourSearchesLabel;
+    private JLabel otherSearchesLabel;
+    private JPanel searchOptionsPanel;
+    private JPanel searchHistoryPanel;
+}
