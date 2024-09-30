0571cbccce8b2a7c0b21e5ca5708631f0c0297c6
Orlando Trujillo-Ortiz <orlandotrujillo573@gmail.com>
2024-09-29 23:10:44 -0700

    final for submission

--- hw3/f24-facts/facts/FactsProject.java
+++ hw3/f24-facts/facts/FactsProject.java
@@ -2,12 +2,11 @@ package facts;
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 
 public class FactsProject extends JFrame{
     private JPanel contentPane;
     private JLabel robertLGlassFactsLabel;
-    private JSeparator seperator1;
-    private JSeparator separator2;
     private JButton nextRandomQuoteButton;
     private JTextField searchBoxTextField;
     private JLabel searchHistoryLabel;
@@ -25,25 +24,30 @@ public class FactsProject extends JFrame{
     private JTextArea searchResults;
     private JTextArea bigQuoteLabel;
     private JPanel bigQuotePanel;
+    private JTextArea yourSearchHistoryTestArea;
     private JPanel searchResultPanel;
     private JTextField searchResult;
     private ButtonGroup modeRadioButtonGroup;
 
     private static final String xmlFile = "f24-facts/facts/data/facts.xml";
-    private FactList list;
+    private final FactList list;
     Parser parser = new Parser(xmlFile);
+    private ArrayList<String> searchHistoryList;
 
     public FactsProject() {
 
-        // Overall Window settings
+        // Overall Window settings and setup
         setTitle("Facts Project");
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setContentPane(contentPane);
-        setSize(850,400);
+        setSize(1080,800);
         list = parser.getFactList();
-        // Main label settings
+        searchHistoryList = new ArrayList<>();
+        searchResults.getCaret().setVisible(false);
         displayRandomQuote();
 
+        // Listener for the "Search" button
+        // Upon press, it will print out the search results and update the search history
         searchButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -62,9 +66,13 @@ public class FactsProject extends JFrame{
                     searchMode = "all";
                 }
                 printSearch(searchText, searchMode);
+                searchHistoryList.add(searchText);
+                printSearchHistory(searchHistoryList);
             }
         });
 
+        // Listener for the "Reset" button
+        // Default selected radio button is "All"
         resetButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -74,6 +82,7 @@ public class FactsProject extends JFrame{
             }
         });
 
+        // Listener for the "Next" button
         nextRandomQuoteButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -82,21 +91,30 @@ public class FactsProject extends JFrame{
         });
     }
 
+
+    /**
+     * Gets a random quote from the FactList and display it on the main big quote.
+     */
     private void displayRandomQuote(){
         Fact randFact = list.getRandom();
-        StringBuilder sb = new StringBuilder();
-        sb.append(randFact.getText());
-        sb.append("\n   -");
-        sb.append(randFact.getAuthor());
-        sb.append("\n   -");
-        sb.append(randFact.getType());
-        bigQuoteLabel.setText(sb.toString());
-        bigQuoteLabel.getCaret().setVisible(false);
+        String sb = randFact.getText() +
+                "\n   —" +
+                randFact.getAuthor() +
+                "\n   —" +
+                randFact.getType();
+        bigQuoteLabel.setText(sb);
+        bigQuoteLabel.getCaret().setVisible(false);//useful
     }
-    ///
-    /// The code beneath has been adapted form FactServlet.java
-    ///
 
+
+    /**
+     * Prints out the search query result from user's input to the screen.
+     * Adapted from the original FactServlet.java code.
+     *
+     * @param searchText The string to search for in within the FactList elements
+     * @param searchMode Defines if all attributes in the Fact list are compared against
+     *                   the searchText or if only a particular attribute is desired.
+     */
     private void printSearch( String searchText, String searchMode) {
         if (searchText != null && !searchText.equals("")){  // Received a search request
             int searchModeVal = FactSearchMode.ALL_VAL; // Default
@@ -112,44 +130,44 @@ public class FactsProject extends JFrame{
             FactList results = list.search(searchText, searchModeVal);
             Fact temp;
             if (results.getSize() == 0){
-                //System.out.println("Not Found!");
                 searchResults.setText("Not Found!");
-            }else{
+            }else{ // Build the string that will be shown to the user
                 String searchResultStr = "";
                 StringBuilder sb = new StringBuilder();
                 for (int i = 0; i < results.getSize() ; i++){
                     temp = results.get(i);
                     sb.append(temp.getText());
-                    sb.append("\n   -");
+                    sb.append("\n   —");
                     sb.append(temp.getAuthor());
-                    sb.append("\n   -");
+                    sb.append("\n   —");
                     sb.append(temp.getType());
                     sb.append("\n");
                     sb.append("\n");
-                    //System.out.println(temp.getText());
-                    //System.out.println(temp.getAuthor());
-                    //System.out.println(temp.getType());
                 }
                 searchResultStr = sb.toString();
                 searchResults.setText(searchResultStr);
+
             }
         }
     }
 
-//    private void printSearcheHistory(ArrayList<String> searchList, ArrayList<String> searchContextList) {
-//        String searchTmp = "";
-//
-//        for (int i = 0; i < searchList.size(); i++){  // The ith search string
-//            searchTmp = searchList.get(i);
-//            out.println("      <li><a href=\"" + thisServlet + "?searchText=" + searchTmp + "&searchMode=both\" >"+searchTmp+"</a></li>");
-//        }
-//
-//        for (int i = 0; i < searchContextList.size(); i++){  // The ith search string
-//            searchTmp = searchContextList.get(i);
-//            out.println("      <li><a href=\"" + thisServlet + "?searchText=" + searchTmp + "&searchMode=both\" >"+searchTmp+"</a></li>");
-//        }
-//
-//    }
+
+    /**
+     * Prints out user's current search history to the search history box.
+     * Adapted from the original FactServlet.java code.
+     *
+     * @param searchList ArrayList of a user's previous searches
+     *
+     */
+    private void printSearchHistory(ArrayList<String> searchList) {
+        String searchTmp = "";
+        yourSearchHistoryTestArea.setText("");
+
+        for (int i = 0; i < searchList.size(); i++){  // The ith search string
+            searchTmp = searchList.get(i) + '\n';
+            yourSearchHistoryTestArea.setText(yourSearchHistoryTestArea.getText() + searchTmp);
+        }
+    }
 
 
 
